b29d3086d2a84ab6eebf12bf36b83ba376394e61
Charlie Wisoff <cwisoff@gmail.com>
2020-05-27 14:59:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,5 +8,11 @@ def countdown (time_left)
   "HAPPY NEW YEAR!"
 end
 
-
+def countdown_with_sleep (time_left)
+  while time_left > 0 do
+    puts "#{time_left} SECOND(S)!"
+    time_left -= 1
     sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
